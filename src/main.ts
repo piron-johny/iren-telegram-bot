dcb522f9304d0eb8e9605d83c268868aa545187f
piron-johny <piron.johny@gmail.com>
2022-08-29 11:06:26 +0300

    [Upd]: Heroku & main config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {AppConfigService} from "./config/config.service";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
+  const appConfig: AppConfigService = app.get(AppConfigService);
+
+  app.enableCors();
+  
+  await app.listen(appConfig.port || 8080);
 }
 bootstrap();
